d05923c8b5e8358927ae52746512c36aa2ffb611
bush688 <32064695+bush688@users.noreply.github.com>
2020-10-02 23:02:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-#git clone https://github.com/Lienol/openwrt-package
+ Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Lienol/openwrt-package
